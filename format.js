478d4300ca1dbaef5a79d5c00e4c4aad7526dc1a
Lukasz Rak <luk1790@gmail.com>
2019-03-23 21:20:35 +0100

    test format

--- format.js
+++ format.js
@@ -3,12 +3,17 @@
  *
  * @param  {Date} date
  * @param  {String} format="DD-MM-YYYY"
+ * @returns {String} Returns the string in correct format.
+ *
  */
 
 const parseNumber = dateNumber =>
   dateNumber.toString().length === 1 ? `0${dateNumber}` : `${dateNumber}`;
 
 function format(date, format = "DD-MM-YYYY") {
+  if(isNaN(date.getTime())){
+    return false;
+  }
   let tempDate = format;
   tempDate = tempDate.replace("DD", `${parseNumber(date.getDate())}`);
   tempDate = tempDate.replace("MM", `${parseNumber(date.getMonth() + 1)}`);
