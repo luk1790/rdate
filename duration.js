dd836cb5223f198bbdf185f8c42a6f25a704505f
Lukasz Rak <luk1790@gmail.com>
2019-03-26 20:26:03 +0100

    duration

--- duration.js
+++ duration.js
@@ -1,5 +1,17 @@
-function duration(){
+function duration(durationMs, date = null){
+    const sec = 1000;
+    const min = 60 * sec;
+    const hour = 60 * min;
+    const day = 24 * hour;
 
+    const result = {day: 0, hour: 0, min: 0, sec: 0, ms: 0};
+    result.day = Math.floor(durationMs/day);
+    result.hour = Math.floor((durationMs - result.day*day)/hour);
+    result.min = Math.floor((durationMs - result.day*day - result.hour*hour)/min);
+    result.sec = Math.floor((durationMs - result.day*day - result.hour*hour - result.min*min)/sec);
+    result.ms = Math.floor(durationMs - result.day*day - result.hour*hour - result.min*min - result.sec*sec);
+    // console.log(result);
+    return result;
 }
 
 module.exports = duration;
--- duration.test.js
+++ duration.test.js
@@ -1,7 +1,19 @@
-var duration = require('./duration');
+var duration = require("./duration");
 
-describe('duration', ()=>{
-    it('',()=>{
-        
-    })
-})
+describe("duration", () => {
+  const sec = 1000;
+  const min = 60 * sec;
+  const hour = 60 * min;
+  const day = 24 * hour;
+
+  it("test", () => {
+    const result = duration(865000100);
+    expect(
+      result.day * day +
+        result.hour * hour +
+        result.min * min +
+        result.sec * sec +
+        result.ms
+    ).toBe(865000100);
+  });
+});
