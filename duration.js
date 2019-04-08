00efde5cb1dd6a5c5f9a525d4f144d3c9478978c
Lukasz Rak <luk1790@gmail.com>
2019-04-08 20:56:37 +0200

    duration

--- duration.js
+++ duration.js
@@ -1,17 +1,89 @@
-function duration(durationMs, date = null){
+var daysInMonth = require('./daysInMonth');
+
+function duration(durationMs, date = null) {
+  const result = { day: 0, hour: 0, min: 0, sec: 0, ms: 0 };
+  if (date) {
+    var futureDate = new Date(date.valueOf() + durationMs);
+    console.log(futureDate);
+
+    // if(date.getMonth() !== futureDate.getMonth()){
+    //   result.day = daysInMonth(date.getFullYear(), date.getMonth())-date.getDate() + futureDate.getDate();
+    // }
+    // if(date.getFullYear() !== futureDate.getFullYear()){
+    //   result.month = 12 - date.getMonth() + 12 * (date.getFullYear()-futureDate.getFullYear()-2) + futureDate.getMonth();
+    // } else {
+    //   result.month = futureDate.getMonth() - date.getMonth()-1;
+    // }
+    // var days = daysInMonth(date.getFullYear(),date.getMonth()+1);
+    
+    result.year = futureDate.getFullYear() - date.getFullYear();
+    // result.month = futureDate.getMonth() - date.getMonth();
+    // result.day = futureDate.getDate() - date.getDate();
+   
+    if(date.getMonth() >= futureDate.getMonth()){
+      result.year = result.year - 1;
+      result.month = 12 - date.getDate() + futureDate.getDate();
+    } else {
+      result.month = futureDate.getMonth() - date.getMonth();
+    }
+
+
+
+    if(date.getDate() >= futureDate.getDate()){
+      result.month = result.month - 1;
+      result.day = daysInMonth(date.getFullYear(), date.getMonth()) - date.getDate() + futureDate.getDate();
+    } else {
+      result.day = futureDate.getDate() - date.getDate();
+    }
+
+
+    if(date.getHours() >= futureDate.getHours()){
+      result.day = result.day - 1;
+      result.hour = 24 - date.getHours() + futureDate.getHours();
+    } else {
+      result.hour = futureDate.getHours() - date.getHours();
+    }
+
+
+    if(date.getMinutes() >= futureDate.getMinutes()){
+      result.hour = result.hour - 1;
+      result.min = 60 - date.getMinutes() + futureDate.getMinutes();
+    } else {
+      result.min = futureDate.getMinutes() - date.getMinutes();
+    }
+
+
+    if(date.getSeconds() >= futureDate.getSeconds()){
+      result.min = result.min - 1;
+      result.sec = 60 - date.getSeconds() + futureDate.getSeconds();
+    } else {
+      result.sec = futureDate.getSeconds() - date.getSeconds();
+    }
+    result.ms = futureDate.getMilliseconds() - date.getMilliseconds();
+  } else {
     const sec = 1000;
     const min = 60 * sec;
     const hour = 60 * min;
     const day = 24 * hour;
 
-    const result = {day: 0, hour: 0, min: 0, sec: 0, ms: 0};
-    result.day = Math.floor(durationMs/day);
-    result.hour = Math.floor((durationMs - result.day*day)/hour);
-    result.min = Math.floor((durationMs - result.day*day - result.hour*hour)/min);
-    result.sec = Math.floor((durationMs - result.day*day - result.hour*hour - result.min*min)/sec);
-    result.ms = Math.floor(durationMs - result.day*day - result.hour*hour - result.min*min - result.sec*sec);
-    // console.log(result);
-    return result;
+    result.day = Math.floor(durationMs / day);
+    result.hour = Math.floor((durationMs - result.day * day) / hour);
+    result.min = Math.floor(
+      (durationMs - result.day * day - result.hour * hour) / min
+    );
+    result.sec = Math.floor(
+      (durationMs - result.day * day - result.hour * hour - result.min * min) /
+        sec
+    );
+    result.ms = Math.floor(
+      durationMs -
+        result.day * day -
+        result.hour * hour -
+        result.min * min -
+        result.sec * sec
+    );
+  }
+  return result;
 }
 
 module.exports = duration;
--- duration.test.js
+++ duration.test.js
@@ -6,14 +6,34 @@ describe("duration", () => {
   const hour = 60 * min;
   const day = 24 * hour;
 
-  it("test", () => {
-    const result = duration(865000100);
-    expect(
-      result.day * day +
-        result.hour * hour +
-        result.min * min +
-        result.sec * sec +
-        result.ms
-    ).toBe(865000100);
+  it("check parsing miliseconds", () => {
+    const variables = [869230100, 899230500, 877230700];
+    variables.forEach(value => {
+      const result = duration(value);
+      expect(
+        result.day * day +
+          result.hour * hour +
+          result.min * min +
+          result.sec * sec +
+          result.ms
+      ).toBe(value);
+    });
   });
+  it("check parsing miliseconds from date", () => {
+    // const variables = [869230100, 899230500, 877230700];
+    // variables.forEach(value => {
+      var dateFirst =  new Date('1990/01/17 17:01:10');
+      console.log(dateFirst);
+      const result = duration(10000003300, dateFirst);
+
+      console.log(result);
+      // expect(
+      //   result.day * day +
+      //     result.hour * hour +
+      //     result.min * min +
+      //     result.sec * sec +
+      //     result.ms
+      // ).toBe(value);
+    });
+  // });
 });
