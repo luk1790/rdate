56815ca84e90f4deaa10d51b7bd8af64c287e132
Lukasz Rak <luk1790@gmail.com>
2019-05-13 14:12:59 +0200

    add test

--- today.test.js
+++ today.test.js
@@ -10,8 +10,12 @@ describe('Today', ()=>{
         expect(isToday(date)).toBeFalsy();
     });
 
-    it('is incorrect type', ()=>{
+    it('is incorrect type - object', ()=>{
         const date = new Date();
+        expect(isToday(date)).toBeFalsy();
+    });
+    it('is incorrect format', ()=>{
+        const date = '10-10-1990';
         expect(isToday(date)).toBeFalsy();
     });
 });
