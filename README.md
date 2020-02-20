a18cae08de755f679470de2cdeeb109dd6e0d3ab
Lukasz <luk1790@gmail.com>
2020-02-20 21:36:58 +0100

    duration fix

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 + [sub](#--sub)
 + [isToday](#--isToday)
 + [isValidDate](#--isValidDate)
++ [duration](#--duration)
 
 ## Functions
 ### - format()
@@ -133,3 +134,26 @@ Sample:
 ```
 
 result: true or false
+
+### - duration()
+
+
+```javascript
+duration(msValue, date)
+```
+options:
+- msValue - value in miliseconds
+- date - date in javaScript (`new Date()`) [optional]
+
+
+Sample:
+```javascript
+ duration(360000)
+ //{ day: 0, hour: 0, min: 6, sec: 0, ms: 0 }
+```
+```javascript
+ duration(360000, new Date('1990/01/17 10:00'))
+ //{ day: 0, hour: 0, min: 6, sec: 0, ms: 0, year: 0, month: 0 }
+```
+
+result: object with value of days, months, years, minutes, hours, seconds and miliseconds
--- duration.js
+++ duration.js
@@ -1,87 +1,84 @@
-var daysInMonth = require('./daysInMonth');
+var daysInMonth = require("./daysInMonth");
+
+function betweenDate(startDate, endDate) {
+  const result = { day: 0, hour: 0, min: 0, sec: 0, ms: 0 };
+  result.year = endDate.getFullYear() - startDate.getFullYear();
+  if (startDate.getMonth() > endDate.getMonth()) {
+    result.year = result.year - 1;
+    result.month = 12 - startDate.getDate() + endDate.getDate();
+  } else {
+    result.month = endDate.getMonth() - startDate.getMonth();
+  }
+
+  if (startDate.getDate() > endDate.getDate() || result.month > 0 ) {
+    result.month = result.month - 1;
+    
+    result.day =
+      daysInMonth(startDate.getFullYear(), startDate.getMonth()) -
+      startDate.getDate() +
+      endDate.getDate();
+  } else {
+   
+    result.day = endDate.getDate() - startDate.getDate();
+  }
+
+  if (startDate.getHours() > endDate.getHours()) {
+    result.day = result.day - 1;
+    result.hour = 24 - startDate.getHours() + endDate.getHours();
+  } else {
+    result.hour = endDate.getHours() - startDate.getHours();
+  }
+
+  if (startDate.getMinutes() > endDate.getMinutes()) {
+    result.hour = result.hour - 1;
+    result.min = 60 - startDate.getMinutes() + endDate.getMinutes();
+  } else {
+    result.min = endDate.getMinutes() - startDate.getMinutes();
+  }
+
+  if (startDate.getSeconds() > endDate.getSeconds()) {
+    result.min = result.min - 1;
+    result.sec = 60 - startDate.getSeconds() + endDate.getSeconds();
+  } else {
+    result.sec = endDate.getSeconds() - startDate.getSeconds();
+  }
+  result.ms = endDate.getMilliseconds() - startDate.getMilliseconds();
+  return result;
+}
+
+function parseDurationMs(durationMs) {
+  let result = { day: 0, hour: 0, min: 0, sec: 0, ms: 0 };
+  const sec = 1000;
+  const min = 60 * sec;
+  const hour = 60 * min;
+  const day = 24 * hour;
+
+  result.day = Math.floor(durationMs / day);
+  result.hour = Math.floor((durationMs - result.day * day) / hour);
+  result.min = Math.floor(
+    (durationMs - result.day * day - result.hour * hour) / min
+  );
+  result.sec = Math.floor(
+    (durationMs - result.day * day - result.hour * hour - result.min * min) /
+      sec
+  );
+  result.ms = Math.floor(
+    durationMs -
+      result.day * day -
+      result.hour * hour -
+      result.min * min -
+      result.sec * sec
+  );
+  return result;
+}
 
 function duration(durationMs, date = null) {
-  const result = { day: 0, hour: 0, min: 0, sec: 0, ms: 0 };
+  let result = { day: 0, hour: 0, min: 0, sec: 0, ms: 0 };
   if (date) {
     var futureDate = new Date(date.valueOf() + durationMs);
-    console.log(futureDate);
-
-    // if(date.getMonth() !== futureDate.getMonth()){
-    //   result.day = daysInMonth(date.getFullYear(), date.getMonth())-date.getDate() + futureDate.getDate();
-    // }
-    // if(date.getFullYear() !== futureDate.getFullYear()){
-    //   result.month = 12 - date.getMonth() + 12 * (date.getFullYear()-futureDate.getFullYear()-2) + futureDate.getMonth();
-    // } else {
-    //   result.month = futureDate.getMonth() - date.getMonth()-1;
-    // }
-    // var days = daysInMonth(date.getFullYear(),date.getMonth()+1);
-    
-    result.year = futureDate.getFullYear() - date.getFullYear();
-    // result.month = futureDate.getMonth() - date.getMonth();
-    // result.day = futureDate.getDate() - date.getDate();
-   
-    if(date.getMonth() >= futureDate.getMonth()){
-      result.year = result.year - 1;
-      result.month = 12 - date.getDate() + futureDate.getDate();
-    } else {
-      result.month = futureDate.getMonth() - date.getMonth();
-    }
-
-
-
-    if(date.getDate() >= futureDate.getDate()){
-      result.month = result.month - 1;
-      result.day = daysInMonth(date.getFullYear(), date.getMonth()) - date.getDate() + futureDate.getDate();
-    } else {
-      result.day = futureDate.getDate() - date.getDate();
-    }
-
-
-    if(date.getHours() >= futureDate.getHours()){
-      result.day = result.day - 1;
-      result.hour = 24 - date.getHours() + futureDate.getHours();
-    } else {
-      result.hour = futureDate.getHours() - date.getHours();
-    }
-
-
-    if(date.getMinutes() >= futureDate.getMinutes()){
-      result.hour = result.hour - 1;
-      result.min = 60 - date.getMinutes() + futureDate.getMinutes();
-    } else {
-      result.min = futureDate.getMinutes() - date.getMinutes();
-    }
-
-
-    if(date.getSeconds() >= futureDate.getSeconds()){
-      result.min = result.min - 1;
-      result.sec = 60 - date.getSeconds() + futureDate.getSeconds();
-    } else {
-      result.sec = futureDate.getSeconds() - date.getSeconds();
-    }
-    result.ms = futureDate.getMilliseconds() - date.getMilliseconds();
+    result = betweenDate(date, futureDate);
   } else {
-    const sec = 1000;
-    const min = 60 * sec;
-    const hour = 60 * min;
-    const day = 24 * hour;
-
-    result.day = Math.floor(durationMs / day);
-    result.hour = Math.floor((durationMs - result.day * day) / hour);
-    result.min = Math.floor(
-      (durationMs - result.day * day - result.hour * hour) / min
-    );
-    result.sec = Math.floor(
-      (durationMs - result.day * day - result.hour * hour - result.min * min) /
-        sec
-    );
-    result.ms = Math.floor(
-      durationMs -
-        result.day * day -
-        result.hour * hour -
-        result.min * min -
-        result.sec * sec
-    );
+    result = parseDurationMs(durationMs)
   }
   return result;
 }
