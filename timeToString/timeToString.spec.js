6c7d47dda5ecc72af2b62aa045fcd3c68b0c4829
luk1790 <luk1790@gmail.com>
2021-05-02 20:13:42 +0200

    [timeToString]

--- timeToString/timeToString.spec.js
+++ timeToString/timeToString.spec.js
@@ -1,11 +1,12 @@
 const timeToString = require('./timeToString');
 describe('timeToString', () => {
-    it('test', () => {
+    it('check some times', () => {
          expect(timeToString('11:29')).toBe('godzina jedynasta minut dwadzieścia dziewięć')
          expect(timeToString('20:29')).toBe('godzina dwudziesta minut dwadzieścia dziewięć')
          expect(timeToString('20:11')).toBe('godzina dwudziesta minut jedenaście')
          expect(timeToString('01:01')).toBe('godzina pierwsza minut jeden')
          expect(timeToString('00:09')).toBe('godzina zero minut dziewięć')
          expect(timeToString('00:45')).toBe('godzina zero minut czterdzieści pięć')
+         expect(timeToString('00:00')).toBe('godzina zero minut zero')
     });
 });
